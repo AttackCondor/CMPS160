a905ef3e42b39ea18bde2d72350710456bd48729
William Connor Koch <wckoch@ucsc.edu>
2019-04-15 15:11:15 -0700

    Assignment 1 init

---
+++ Assignment 1/src/graphics/geometry/shapes/triangle.js
@@ -0,0 +1,41 @@
+/**
+ * Specifies a triangle. A subclass of geometry.
+ *
+ * @author Lucas N. Ferreira
+ * @this {Triangle}
+ */
+class Triangle extends Geometry {
+  /**
+   * Constructor for Triangle.
+   *
+   * @constructor
+   * @param {Shader} shader Shading object used to shade geometry
+   * @returns {Triangle} Triangle created
+   */
+  constructor(shader , xCor, yCor, size) {
+      super(shader);
+
+	  this.xCor = xCor;
+	  this.yCor = yCor;
+	  this.size = size;
+      this.vertices = this.generateTriangleVertices(xCor, yCor, size);
+      this.faces = {0: this.vertices};
+
+      // CALL THIS AT THE END OF ANY SHAPE CONSTRUCTOR
+      this.interleaveVertices();
+  }
+
+  generateTriangleVertices(double, double, double) {
+      var vertices = []
+
+      var vertex1 = new Vertex(xCor, yCor+size, 0.0);
+      var vertex2 = new Vertex(0.0 , -0.25, 0.0);
+      var vertex3 = new Vertex( 0.0,   0.25, 0.0);
+
+      vertices.push(vertex1);
+      vertices.push(vertex2);
+      vertices.push(vertex3);
+
+      return vertices;
+  }
+}
