ad7727bc032cfdac5cae7c65ce361e5a1a95f6a9
William Connor Koch <wmconnorkoch@gmail.com>
2019-04-18 00:04:10 -0700

    Tidied up the front end

--- Assignment 1/src/input.js
+++ Assignment 1/src/input.js
@@ -10,14 +10,17 @@ class InputHandler {
     /**
      * Initializes the event handling functions within the program.
      */
+
     constructor(canvas, scene) {
       this.canvas = canvas;
       this.scene = scene;
 
       _inputHandler = this;
-
+      var mouseheld = false;
       // Mouse Events
-      this.canvas.onmousedown = function(ev) { _inputHandler.click(ev) };
+      this.canvas.onmousedown = function(ev) { this.mouseheld = true; _inputHandler.click(ev);};
+      this.canvas.onmouseup   = function(ev) { this.mouseheld = false;};
+      this.canvas.onmousemove = function(ev) { if(this.mouseheld){_inputHandler.click(ev)} };
       document.getElementById("clear").onclick = function() { _inputHandler.scene.clearGeometries();};
     }
 
@@ -27,25 +30,27 @@ class InputHandler {
     click(ev) {
         // Print x,y coordinates.
         console.log(ev.clientX, ev.clientY);
-        var x = (ev.clientX - (canvas.height/2))/(canvas.height/2);
-        var y = ((canvas.height/2) - ev.clientY)/(canvas.height/2);
-        var size = (document.getElementById("Size").value)/100;
-        var red = document.getElementById("Red").value;
-        var green = document.getElementById("Green").value;
-        var blue = document.getElementById("Blue").value;
-        var segments = document.getElementById("Segments").value;
-        var color = [red, green, blue];
 
-        if(document.getElementById("triangle").checked){
-          var shape = new Triangle(shader, x, y, size, color);
-        }
-        else if(document.getElementById("square").checked){
-          var shape = new Square(shader, x, y, size, color);
-        }
-        else if(document.getElementById("circle").checked){
-          var shape = new Circle(shader, x, y, size, color, segments);
+          var x = (ev.clientX - (canvas.height/2))/(canvas.height/2);
+         var y = ((canvas.height/2) - ev.clientY)/(canvas.height/2);
+         var size = (document.getElementById("Size").value)/100;
+         var red = document.getElementById("Red").value;
+         var green = document.getElementById("Green").value;
+         var blue = document.getElementById("Blue").value;
+         var segments = document.getElementById("Segments").value;
+         var color = [red, green, blue];
+
+         if(document.getElementById("triangle").checked){
+           var shape = new Triangle(shader, x, y, size, color);
+         }
+         else if(document.getElementById("square").checked){
+           var shape = new Square(shader, x, y, size, color);
+         }
+         else if(document.getElementById("circle").checked){
+           var shape = new Circle(shader, x, y, size, color, segments);
         }
         this.scene.addGeometry(shape);
+      
     }
 
 }
