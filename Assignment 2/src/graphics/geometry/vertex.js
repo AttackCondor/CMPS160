9841df57afc798d3c7da1172e29fa84519757a9f
William Connor Koch <wckoch@ucsc.edu>
2019-05-06 21:46:19 -0700

    asg2

---
+++ Assignment 2/src/graphics/geometry/vertex.js
@@ -0,0 +1,17 @@
+/**
+ * Specifies a vertex. Currently only contains the vertex's position.
+ *
+ * @author Lucas N. Ferreira
+ * @this {Vertex}
+ */
+class Vertex {
+  constructor(x, y, z, color) {
+    this.point  = new Vector3([x, y, z]);
+    if(color){color = color.map(x => x/255);
+    this.color  = [color[0], color[1], color[2], 1.0];}
+    else this.color = [0,255,0];
+
+      // This class can be extended to support other attributes such as
+      // normals and UV coordinates.
+  }
+}
