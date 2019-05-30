dc884745dfd2f1ca93ab25366d6b94486266fda9
William Connor Koch <wmconnorkoch@gmail.com>
2019-05-30 15:40:13 -0700

    Movement is working

--- Final Project/src/graphics/geometry/shapes/ship.js
+++ Final Project/src/graphics/geometry/shapes/ship.js
@@ -18,11 +18,16 @@ class Ship extends Geometry {
     this.xMom = 0;
     this.yMom = 0;
     this.color = [0, 255, 0];
+    this.lrot = 0;
+    this.rrot = 0;
+    this.dirVec = new Vector3([0,1,0]);
+    this.rotMatrix = new Matrix4();
+    this.transMatrix = new Matrix4();
+    this.modelMatrix = new Matrix4();
 
     this.vertices = this.generateShipVertices();
     this.faces = { 0: this.vertices };
 
-    this.modelMatrix = new Matrix4();
 
     // CALL THIS AT THE END OF ANY SHAPE CONSTRUCTOR
     this.interleaveVertices();
@@ -62,8 +67,19 @@ class Ship extends Geometry {
   }
 
   render() {
+    //Set the rotation matrix to the desired rotation
+    this.rotMatrix.multiply(new Matrix4().setRotate(this.lrot+this.rrot, 0, 0, 1));
 
+    //Set the direction vector equal to the forward direction of the ship after rotating
+    this.dirVec = this.rotMatrix.multiplyVector3(new Vector3([0,1,0]));
+
+    //Set the translation matrix equal to the x momentum and y momentum
+    this.transMatrix.multiply(new Matrix4().setTranslate(this.xMom, this.yMom, 0));
+
+    //Factor these matrices into the model matrix
+    this.modelMatrix.multiply(this.transMatrix);
+    this.modelMatrix.multiply(this.rotMatrix);
     this.shader.setUniform("u_ModelMatrix", this.modelMatrix.elements);
-    
+    this.modelMatrix = new Matrix4();
   }
 }
--- Final Project/src/input.js
+++ Final Project/src/input.js
@@ -19,12 +19,14 @@ class InputHandler {
     this.image = null;
 
     // Mouse Events
-    this.canvas.onmousedown = function(ev) { this.mouseheld = true; _inputHandler.click(ev);};
-    this.canvas.onmouseup   = function() { this.mouseheld = false;};
-    this.canvas.onmousemove = function(ev) { if(this.mouseheld){_inputHandler.click(ev)} };
+    this.canvas.onmousedown = function (ev) { this.mouseheld = true; _inputHandler.click(ev); };
+    this.canvas.onmouseup = function () { this.mouseheld = false; };
+    this.canvas.onmousemove = function (ev) { if (this.mouseheld) { _inputHandler.click(ev) } };
 
     //button events
-    document.getElementById("clear").onclick = function() { _inputHandler.scene.clearGeometries();};
+    document.getElementById("clear").onclick = function () { _inputHandler.scene.clearGeometries(); };
+    document.addEventListener('keydown', function (ev) { _inputHandler.keyDown(ev); }, false);
+    document.addEventListener('keyup', function (ev) { _inputHandler.keyUp(ev); }, false);
 
   }
 
@@ -35,33 +37,53 @@ class InputHandler {
     // Print x,y coordinates.
     console.log(ev.clientX, ev.clientY);
     //Convert coordinates to webgl style
-     var x = (ev.clientX - (canvas.height/2))/(canvas.height/2);
-     var y = ((canvas.height/2) - ev.clientY)/(canvas.height/2);
-
-     var size = (document.getElementById("Size").value)/100;
-     var red = document.getElementById("Red").value;
-     var green = document.getElementById("Green").value;
-     var blue = document.getElementById("Blue").value;
-     var segments = document.getElementById("Segments").value;
-     var color = [red, green, blue];
-     if(document.getElementById("colorType").value == "Rainbow!") color = null;
-
-     if(document.getElementById("spinsquare").checked){
-       var shape = new Square(shader2, x, y, size, color);
-     }
-     else if(document.getElementById("fluctriangle").checked){
-       var shape = new Triangle(shader2, x, y, size, color);
-     }
-     else if(document.getElementById("randcircle").checked){
-       var shape = new Circle(shader2, x, y, size, color, segments);
+    var x = (ev.clientX - (canvas.height / 2)) / (canvas.height / 2);
+    var y = ((canvas.height / 2) - ev.clientY) / (canvas.height / 2);
+  }
+  keyUp(ev) {
+    var keyName = event.key;
+    //console.log("key up", keyName);
+    if (keyName == "a") {
+      //left
+      _inputHandler.scene.geometries[0].lrot = 0;
+      console.log(_inputHandler.scene.geometries[0].dirVec.elements);
     }
-    else if(document.getElementById("tiltcube").checked && !this.image){
-      var shape = new Cube(shader2, x, y, size, color);
-   }
-   else if(document.getElementById("tiltcube").checked){
-    var shape = new TexCube(shader, x, y, size, this.image);
- }
-    this.scene.addGeometry(shape);
+    else if (keyName == "d") {
+      //right
+      _inputHandler.scene.geometries[0].rrot = 0;
+      console.log(_inputHandler.scene.geometries[0].dirVec.elements);
+    }
+    else if (keyName == "w") {
+      //up
+    }
+    else if (keyName == "s") {
+      //down
+    }
+  }
+
+  keyDown(ev) {
+    var keyName = event.key;
+    // console.log("key down", keyName);
+    if (keyName == "a") {
+      //left
+      _inputHandler.scene.geometries[0].lrot = 3;
+    }
+    else if (keyName == "d") {
+      //right
+      _inputHandler.scene.geometries[0].rrot = -3;
+      //console.log(_inputHandler.scene.geometries[0].dirVec.elements);
+    }
+    else if (keyName == "w") {
+      //up
+      console.log(_inputHandler.scene.geometries[0].dirVec.elements);
+      _inputHandler.scene.geometries[0].xMom += _inputHandler.scene.geometries[0].dirVec.elements[0] * .01;
+      _inputHandler.scene.geometries[0].yMom += _inputHandler.scene.geometries[0].dirVec.elements[1] * .01;
+      console.log(_inputHandler.scene.geometries[0].xMom, _inputHandler.scene.geometries[0].yMom);
+    }
+    else if (keyName == "s") {
+      //down
+    }
+
   }
 
 
